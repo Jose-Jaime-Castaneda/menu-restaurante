7ea206dd5149d9d1869c6c5047e8d957f0784f88
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-20 11:46:59 -0600

    Adding the form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
+    const { pathname } = useLocation()
+
+    const isHome = useMemo(() => pathname === '/', [pathname])
+
     return (
         <header className=" bg-slate-800">
             <div className=" mx-auto container px-5 py-16">
@@ -25,6 +30,46 @@ export default function Header() {
                         >Favoritos</NavLink>
                     </nav>
                 </div>
+                {
+                    isHome && (
+                        <form className=" md:w-1/2 lg:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                            <div className=" space-y-4">
+                                <label
+                                    htmlFor="ingredient"
+                                    className="block text-white uppercase font-extrabold text-lg"
+                                >Nombre o Ingredientes</label>
+
+                                <input
+                                    type="text"
+                                    name="ingredient"
+                                    id="ingredient"
+                                    className="p-3 w-full rounded-lg focus:outline-none"
+                                    placeholder="Nombre o Ingrete. Ej, Vodka, Café"
+                                />
+                            </div>
+
+                            <div className=" space-y-4">
+                                <label
+                                    htmlFor="ingredient"
+                                    className="block text-white uppercase font-extrabold text-lg"
+                                >Categoria</label>
+
+                                <select
+                                    name="ingredient"
+                                    id="ingredient"
+                                    className="p-3 w-full rounded-lg focus:outline-none"
+                                >
+                                    <option value="">-- Seleccione una Categoria --</option>
+                                </select>
+                            </div>
+
+                            <input
+                                type="submit"
+                                value="Buscar resetas"
+                                className=" cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase" />
+                        </form>
+                    )
+                }
             </div>
         </header>
     );
