dd2ffcb58503de95e83de602e95e72cb6f5286d0
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-21 12:10:37 -0600

    Adding 2 parameters to the API query

--- src/services/recipeService.ts
+++ src/services/recipeService.ts
@@ -14,5 +14,9 @@ export async function getCategories() {
 }
 
 export async function getRecipies(filters: SearchFilter) {
-  console.log("hola");
+  const { data } = await axios(
+    `www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filters.category}&i=${filters.ingredient}`
+  );
+
+  console.log(data);
 }
