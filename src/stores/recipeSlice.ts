b4ef57989bf72cf55cdd87e10176558e7b48a088
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-21 10:43:51 -0600

    Creating the function to do the api call and using it in the slice

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,11 +1,17 @@
 import { StateCreator } from "zustand";
+import { getCategories } from "../services/recipeService";
 
 type Category = {};
 
 export type RecipiesSliceType = {
   categories: Category[];
+  fetchCategories: () => Promise<void>;
 };
 
 export const createRecipiesSlice: StateCreator<RecipiesSliceType> = () => ({
   categories: [],
+
+  fetchCategories: async () => {
+    getCategories();
+  },
 });
