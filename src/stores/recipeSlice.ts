bb702631efc5c6809f864f8597998be90a2a1b76
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-22 10:55:37 -0600

    Adding drinks to the state

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,15 +1,19 @@
 import { StateCreator } from "zustand";
 import { getCategories, getRecipies } from "../services/recipeService";
-import { Categories, SearchFilter } from "../types";
+import { Categories, Drinks, SearchFilter } from "../types";
 
 export type RecipiesSliceType = {
   categories: Categories;
+  drinks: Drinks;
   fetchCategories: () => Promise<void>;
   searchRecipies: (searchFilters: SearchFilter) => Promise<void>;
 };
 
 export const createRecipiesSlice: StateCreator<RecipiesSliceType> = (set) => ({
   categories: {
+    drinks: [],
+  },
+  drinks: {
     drinks: [],
   },
 
@@ -21,6 +25,9 @@ export const createRecipiesSlice: StateCreator<RecipiesSliceType> = (set) => ({
   },
   searchRecipies: async (searchFilters) => {
     const drinks = await getRecipies(searchFilters);
-    console.log(drinks);
+    //console.log(drinks);
+    set({
+      drinks,
+    });
   },
 });
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 import {
   CategoriesAPIResponseSchema,
+  DrinksAPIResponse,
   SearchFilterSchema,
 } from "../schema/recipies-schema";
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
 export type SearchFilter = z.infer<typeof SearchFilterSchema>;
+export type Drinks = z.infer<typeof DrinksAPIResponse>;
