a000f3556a2c25f5c214adb445011126ab6d580d
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-22 11:20:13 -0600

    Adding more styles to the cards

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -8,8 +8,12 @@ export default function DrinkCard({ drink }: DrinkCardProps) {
 
     return (
         <div className=" border shadow-lg">
-            <div>
-                <img src={drink.strDrinkThumb} alt={`Imagen de ${drink.strDrink}`} />
+            <div className="overflow-hidden">
+                <img
+                    src={drink.strDrinkThumb}
+                    alt={`Imagen de ${drink.strDrink}`}
+                    className="hover:scale-125 transition-tranform hover:rotate-2"
+                />
             </div>
 
             <div className=" p-5">
