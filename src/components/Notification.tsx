9f87f460ea84dad0ebf413fa658cd979260a1a61
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-23 12:13:57 -0600

    Now the function to hide Notification is used in the notification component

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -5,7 +5,7 @@ import { Transition } from '@headlessui/react'
 import { useAppStore } from '../stores/useAppStore'
 
 export default function Notification() {
-    const { notification } = useAppStore();
+    const { notification, hideNotification } = useAppStore();
 
     return (
         <div
@@ -41,7 +41,7 @@ export default function Notification() {
                                     <button
                                         type="button"
                                         className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                        onClick={() => { }}
+                                        onClick={hideNotification}
                                     >
                                         <span className="sr-only">Cerrar</span>
                                         <XMarkIcon className="h-5 w-5" aria-hidden="true" />
@@ -51,7 +51,7 @@ export default function Notification() {
                         </div>
                     </div>
                 </Transition>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
