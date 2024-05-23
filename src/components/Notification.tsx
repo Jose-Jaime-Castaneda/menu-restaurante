4f54d3dde23df09b01f5abfb7d9f783b46edb7cb
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-23 11:51:50 -0600

    Rendering Icon depending on the state if error or not

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -27,11 +27,15 @@ export default function Notification() {
                         <div className="p-4">
                             <div className="flex items-start">
                                 <div className="flex-shrink-0">
-
+                                    {notification.error ? (
+                                        <XCircleIcon className='h-6 w-6 text-red-400' aria-hidden="true" />
+                                    ) : (
+                                        <CheckCircleIcon className='h-6 w-6 text-green-400' aria-hidden="true" />
+                                    )}
                                 </div>
                                 <div className="ml-3 w-0 flex-1 pt-0.5">
                                     <p className="text-sm font-medium text-gray-900">Notificación</p>
-                                    <p className="mt-1 text-sm text-gray-500"></p>
+                                    <p className="mt-1 text-sm text-gray-500">{notification.text}</p>
                                 </div>
                                 <div className="ml-4 flex flex-shrink-0">
                                     <button
